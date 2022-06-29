5e79853db5b16c9ee31253f6adf226e13f0e920d
Jenny Shalai <jenny.shalai@gmail.com>
2022-06-29 08:08:49 -0400

    project init

---
+++ follow-along-coder-swag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  follow-along-coder-swag
+//
+//  Created by Jenny Shalai on 6/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
